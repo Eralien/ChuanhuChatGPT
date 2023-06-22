51a8e33fc7f9d40bf49a57d5b7a3aa6f23f19919
Eralien <eralienfeng@gmail.com>
2023-06-22 11:53:02 +0800

    feat: align docker tag as latest

--- run_docker.sh
+++ run_docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker stop chatgpt && docker rm chatgpt && docker build -t chuanhuchatgpt:0.1 .
+docker stop chatgpt && docker rm chatgpt && docker build -t chuanhuchatgpt:latest .
 
 docker run -d --name chatgpt \
 	-v `pwd`/history:/app/history \
     --add-host=host.docker.internal:host-gateway \
     -p 7860:7860 \
     --restart unless-stopped \
-	chuanhuchatgpt:0.1
+	chuanhuchatgpt:latest
