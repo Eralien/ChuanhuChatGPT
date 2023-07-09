0d59b6dd4a96a76acca665a5ad38f79914c19df7
Siyuan_Feng <eralienfeng@gmail.com>
2023-07-09 13:33:24 +0800

    <fix>: win docker need no add internal host arg

--- run_docker.bat
+++ run_docker.bat
@@ -1,10 +1,9 @@
 @echo off
 
-docker stop chatgpt & docker rm chatgpt & docker build -t chuanhuchatgpt:0.1 .
+docker stop chatgpt & docker rm chatgpt & docker build -t chuanhuchatgpt:latest .
 
 docker run -d --name chatgpt ^
 	-v %cd%/history:/app/history ^
-    --add-host=host.docker.internal:host-gateway ^
     -p 7860:7860 ^
     --restart unless-stopped ^
-	chuanhuchatgpt:0.1
+	chuanhuchatgpt:latest
